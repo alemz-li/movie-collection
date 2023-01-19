d2cd1471610e523c59f7a4661eb7f22cfa4e9c02
Alejandro Meza <alemz-li@outlook.com>
2023-01-18 19:46:39 -0800

    Changed axios endpoint for client requests. Added static files for express to render react files

--- server/src/app.js
+++ server/src/app.js
@@ -5,12 +5,18 @@ import morgan from 'morgan'
 import cors from 'cors'
 import corsOptions from './config/corsOptions.js'
 import cookieParser from 'cookie-parser'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 dotenv.config()
 
 import collectionRoutes from './api/routes/collection.routes.js'
 import authRoutes from './api/routes/auth.routes.js'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// Frontend
+app.use(express.static(join(__dirname, '../../client/dist')))
 
 // Middlerwares
 if(process.env.NODE_ENV === 'development'){
@@ -29,13 +35,13 @@ app.use(cookieParser())
 
 // Endpoints
 app.get('/', (_req, res) => {
-  res.json({message: 'Hello from the server'})
+  res.sendFile(join(__dirname, '../../client/dist/index.html'))
 })
 
 app.use('/api/collection', collectionRoutes)
 app.use('/api/auth', authRoutes)
 
 app.all('*', (_req, res) => {
-  res.status(404).json({message: 'Not found'})
+  res.sendFile(join(__dirname, '../../client/dist/index.html'))
 })
 export default app
