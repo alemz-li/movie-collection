84d45d926557dbc27ad7a38ad1d1240ea90fc0a0
Alejandro Meza <alemz-li@outlook.com>
2023-01-18 20:24:04 -0800

    Removed react-query dev tools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthProvider } from './context/AuthProvider'
 
 const queryClient = new QueryClient()
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProvider>
         <App />
       </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 )
